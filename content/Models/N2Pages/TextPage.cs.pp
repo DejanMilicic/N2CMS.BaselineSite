0a88a4eb970f28258e8bd022d852bb4cc46c608c
Dejan Milicic <dejan.milicic@gmail.com>
2015-03-28 01:07:01 +0100

    Text page

--- content/Models/N2Pages/TextPage.cs.pp
+++ content/Models/N2Pages/TextPage.cs.pp
@@ -1,9 +1,27 @@
 ﻿
 namespace $rootnamespace$.Models.N2Pages
 {
+	using N2;
+	using N2.Details;
+	using N2.Integrity;
+
+	using $rootnamespace$.N2Baseline;
 	using $rootnamespace$.N2Baseline.Models;
 
+	[PageDefinition(Title = "TextPage", IconClass = "icon-align-justify")]
+
+	[RestrictParents(typeof(StartPage), typeof(TextPage))]
+	//[AvailableZone("Content", "Content")]
 	public class TextPage : PublicPageModelBase
 	{
+		#region Tab : Content
+
+		[EditableText(Title = "Heading", DefaultValue = "", SortOrder = 100, ContainerName = Tabs.Content.Name)]
+		public virtual string Heading { get; set; }
+
+		[EditableFreeTextArea(Title = "Text", DefaultValue = "", SortOrder = 110, ContainerName = Tabs.Content.Name)]
+		public virtual string Text { get; set; }
+
+		#endregion
 	}
 }
